feede15c838f0f3de3b8b877ad1a9a4f7267ae9f
Jason Mansfield <jason@lub-dub.org>
2024-08-11 14:57:53 -0700

    TSProtosInDir

--- proto.go
+++ proto.go
@@ -3,6 +3,7 @@ package mageutil
 import (
 	"fmt"
 	"path/filepath"
+	"runtime"
 	"strings"
 
 	"github.com/magefile/mage/sh"
@@ -39,6 +40,50 @@ func GoProtosInDir(dir, opt string) error {
 		destPath := filepath.Join(dir, strings.TrimSuffix(srcPath, ".proto")+".pb.go")
 		if err := GoProto(destPath, srcPath, dir, opt); err != nil {
 			return fmt.Errorf("running protoc on %s: %w", srcPath, err)
+		}
+	}
+	return nil
+}
+
+func TSProtosInDir(destDir, srcDir string) error {
+	if err := HasExec("protoc"); err != nil {
+		return err
+	}
+	plugin := filepath.Join(srcDir, "node_modules/.bin/protoc-gen-es")
+	if runtime.GOOS == "windows" {
+		plugin += ".cmd"
+	}
+	if err := HasFiles(plugin); err != nil {
+		return err
+	}
+	protoDestDir := filepath.Join(destDir, "pb")
+	if err := Mkdir(protoDestDir); err != nil {
+		return fmt.Errorf("creating %s: %w", protoDestDir, err)
+	}
+	protos, err := DirGlob(srcDir, "*.proto")
+	if err != nil {
+		return fmt.Errorf("matching files: %w", err)
+	}
+	for _, srcFile := range protos {
+		baseName := strings.TrimSuffix(filepath.Base(srcFile), ".proto")
+		destFile := filepath.Join(protoDestDir, baseName+"_pb.js")
+		// srcFile = filepath.Join(srcDir, srcFile)
+		newer, err := target.Path(destFile, srcFile)
+		if err != nil {
+			return fmt.Errorf("testing %s vs %s: %w", srcFile, destFile, err)
+		}
+		if !newer {
+			continue
+		}
+		VerboseF("generating proto %s -> %s\n", srcFile, destFile)
+		err = sh.Run("protoc",
+			"--plugin", "protoc-gen-es="+plugin,
+			"-I", srcDir,
+			"--es_out", protoDestDir,
+			srcFile,
+		)
+		if err != nil {
+			return fmt.Errorf("generating proto %s -> %s\n: %w", srcFile, destFile, err)
 		}
 	}
 	return nil
