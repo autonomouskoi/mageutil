baee99cfe3b53c896dfaa7f5ae3cb541199b8d74
Jason Mansfield <jason@lub-dub.org>
2025-03-25 20:12:41 -0700

    TinyGoProto

--- proto.go
+++ proto.go
@@ -2,9 +2,14 @@ package mageutil
 
 import (
 	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"os/exec"
 	"path/filepath"
 	"runtime"
 	"strings"
+	"sync"
 
 	"github.com/magefile/mage/sh"
 	"github.com/magefile/mage/target"
@@ -89,5 +94,79 @@ func TSProtosInDir(destDir, srcDir string) error {
 			return fmt.Errorf("generating proto %s -> %s\n: %w", srcFile, destFile, err)
 		}
 	}
+	return nil
+}
+
+var pcggl string
+var pcgglOnce sync.Once
+
+func TinyGoProto(
+	dstPath string, // the final .pb.go file
+	srcPath string, // the .proto file
+	includeDir string, // for -I
+) error {
+	newer, err := target.Path(dstPath, srcPath)
+	if err != nil {
+		return fmt.Errorf("testing %s vs %s: %w", srcPath, dstPath, err)
+	}
+	if !newer {
+		return nil
+	}
+
+	pcgglOnce.Do(func() {
+		var err error
+		pcggl, err = exec.LookPath("protoc-gen-go-lite")
+		if err != nil {
+			pcggl = ""
+			return
+		}
+	})
+
+	dstDir := filepath.Join(filepath.Dir(dstPath), "protoc-out")
+	if err := Mkdir(dstDir); err != nil {
+		return fmt.Errorf("creating %s: %w", dstDir, err)
+	}
+	defer sh.Rm(dstDir)
+
+	err = sh.Run("protoc",
+		"--plugin", pcggl,
+		"--go-lite_opt", "features=marshal+unmarshal+size+equal+clone",
+		"-I", includeDir,
+		"--go-lite_out", dstDir,
+		srcPath,
+	)
+	if err != nil {
+		return err
+	}
+
+	err = filepath.WalkDir(dstDir, func(path string, d fs.DirEntry, _ error) error {
+		if !strings.HasSuffix(d.Name(), ".pb.go") {
+			return nil
+		}
+		outfh, err := os.Create(dstPath)
+		if err != nil {
+			return fmt.Errorf("creating %s: %w", dstPath, err)
+		}
+		defer outfh.Close()
+
+		inFile := path
+		infh, err := os.Open(inFile)
+		if err != nil {
+			return fmt.Errorf("opening %s: %w", inFile, err)
+		}
+		defer infh.Close()
+
+		if _, err := io.Copy(outfh, infh); err != nil {
+			return fmt.Errorf("copying %s -> %s: %w", inFile, dstPath, err)
+		}
+		if err := outfh.Sync(); err != nil {
+			return fmt.Errorf("syncing %s: %w", dstPath, err)
+		}
+		return filepath.SkipAll
+	})
+	if err != nil {
+		return fmt.Errorf("copying generated proto: %w", err)
+	}
+
 	return nil
 }
